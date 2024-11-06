d682f7bf8d86a9bb9a9a7e890f05ad2a66cd285f
Michael,Lee <Guan-Wei@users.noreply.github.com>
2024-11-07 00:06:23 +0800

    Update and rename SelectionChange application to Selection_Change.vb
    Change file type( .vb).

--- Selection_Change.vb
+++ Selection_Change.vb
@@ -1,6 +1,8 @@
 ' 6.3.3 SelectionChange事件
 ' From <Excel 2016 Power Programming with VBA><中文版> Book
 ' Put this VBA code in your sheet(工作表) of excel.
+
+
 Private Sub Worksheet_SelectionChange(ByVal Target As Range)
   Cells.Interior.ColorIndex = xlNone
   With ActiveCell
